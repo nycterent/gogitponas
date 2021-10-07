9a0068792049c3be5d936dc436ab1945a06a55fa
Martin Brown <martin@surfsharkbiz.com>
2021-10-07 08:34:45 +0300

    implemented slack target

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"gogitponas/gitlab"
 	"gogitponas/registry"
 	"gogitponas/rocketchat"
+	"gogitponas/slack"
 	"log"
 	"os"
 	"strings"
@@ -22,14 +23,11 @@ func main() {
 	gitlabToken := os.Getenv("GITLAB_TOKEN")
 	gitlabProjects := strings.Split(os.Getenv("GITLAB_PROJECTS"), ",")
 
-	// slack_hook := os.Getenv("SLACK_HOOK")
-	// slack_channel := os.Getenv("SLACK_CHANNEL")
-
 	g := gitlab.New(gitlabToken, gitlabURL)
 
 	notificationTargets := registry.New(strings.Split(os.Getenv("NOTIFICATION_TARGETS"), ","))
 
-	notificationTargets.Register("slack", &Slack{})
+	notificationTargets.Register("slack", &Slack{sc: slack.New(os.Getenv("SLACK_HOOK"))})
 	notificationTargets.Register("rocket", &Rocket{rc: rocketchat.New(os.Getenv("ROCKET_HOOK"))})
 
 	for _, project := range gitlabProjects {
--- slack_target.go
+++ slack_target.go
@@ -1,23 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"gogitponas/gitlab"
 	"gogitponas/registry"
+	"gogitponas/slack"
+	"log"
 )
 
 var _ registry.Callback = (*Slack)(nil)
 
 // Slack is stub for slack message and client
 type Slack struct {
-	name string
+	gmi gitlab.GitlabMergeInformation
+	sc  *slack.Slack
 }
 
 // Send implements sending message to slack
 func (s Slack) Send() {
-	fmt.Printf("jee, slack, %v\n", s.name)
+	err := s.sc.Send(slack.Message{
+		Text: s.gmi.Title,
+		Attachments: []slack.MessageAttachment{
+			{
+				Title:     s.gmi.Reference,
+				TitleLink: s.gmi.MRURL,
+				Text:      s.gmi.Author,
+			},
+		},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
 
 // Set casts interface to slack message
 func (s *Slack) Set(i interface{}) {
-	s.name = i.(string)
+	s.gmi = i.(gitlab.GitlabMergeInformation)
 }
